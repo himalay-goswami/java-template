6a979c8099df5580c87ac9c755093b86c930d8c7
Himalay Goswami <himalayagswm@gmail.com>
2025-08-19 14:30:49 +0530

    base commit

---
+++ src/main/java/work/higo/App.java
@@ -0,0 +1,8 @@
+package work.higo;
+
+public class App {
+
+	public static void main(String[] args) {
+		// This is the entry point of the application.
+	}
+}
---
+++ target/generated-sources/archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/App.java
@@ -0,0 +1,7 @@
+package it.pkg;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
